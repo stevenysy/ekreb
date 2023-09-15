05e35e6e928c7c11df4dec88b11c20b83ca9fed1
stevenysy <stevenjxhc@gmail.com>
2023-09-14 22:34:07 -0500

    add scramble function

---
+++ ekreb-backend/src/config.js
@@ -0,0 +1,2 @@
+export const WORDS_MAX_LEN = 7;
+export const WORD_URL = "https://wordsapiv1.p.rapidapi.com/words/";
--- ekreb-backend/src/model.js
+++ ekreb-backend/src/model.js
@@ -1,14 +1,20 @@
-// const axios = require("axios");
 import axios from "axios";
+import { WORDS_MAX_LEN, WORD_URL } from "./config.js";
 
+/**
+ * Calls a word dictionary API to get a random word. Sends an object containing the word and
+ * the word's definition, part of speech, and frequency used
+ * @param {Object} req request object passed in by Express
+ * @param {Object} res response object passed in by Express
+ */
 export const getWordData = async function (req, res) {
   try {
-    const response = await axios.request({
+    const { data } = await axios.request({
       method: "GET",
-      url: "https://wordsapiv1.p.rapidapi.com/words/",
+      url: `${WORD_URL}`,
       params: {
         random: "true",
-        lettersMax: "7",
+        lettersMax: `${WORDS_MAX_LEN}`,
         hasDetails: "definitions",
       },
       headers: {
@@ -17,24 +23,39 @@ export const getWordData = async function (req, res) {
       },
     });
 
-    const data = response.data;
     const word = {
       word: data.word,
+      scrambledWord: scramble(data.word),
       definition: data.results[0].definition,
       partOfSpeech: data.results[0].partOfSpeech,
       frequency: data.frequency,
     };
 
     res.send({ status: "success", data: word });
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    res.send({ status: "failed", message: err.message });
   }
 };
 
-export const scramble = function (original) {
+/**
+ * Scrambles the word passed in
+ * @param {String} original the original, unscrambled word
+ * @returns a scrambled version of the original word
+ */
+const scramble = function (original) {
   const wordArr = original.split("");
 
-  for (let i = wordArr.length - 1; i > 0; i++) {
-    const temp = Math.floor();
+  // swap each letter in the word with a random other letter
+  for (let i = wordArr.length - 1; i > 0; i--) {
+    const rand = Math.floor(Math.random() * (i + 1));
+    const temp = wordArr[i];
+    wordArr[i] = wordArr[rand];
+    wordArr[rand] = temp;
   }
+
+  // make sure the word is scrambled
+  const scrambled = wordArr.join("");
+  if (scrambled === original) return scramble(scrambled);
+
+  return scrambled;
 };

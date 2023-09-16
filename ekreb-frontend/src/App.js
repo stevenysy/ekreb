1bca16107511cf40bdd7af5e2df311f15477706e
stevenysy <stevenjxhc@gmail.com>
2023-09-16 01:40:30 -0500

    initial commit

---
+++ ekreb-frontend/src/App.js
@@ -0,0 +1,16 @@
+import GetWordBtn from "./GetWordBtn";
+
+function App() {
+  const title = "ekreb";
+
+  return (
+    <div className="App">
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+      <GetWordBtn />
+    </div>
+  );
+}
+
+export default App;
---
+++ ekreb-frontend/src/GetWordBtn.js
@@ -0,0 +1,9 @@
+const GetWordBtn = () => {
+  return (
+    <nav className="btn">
+      <button>Get A Word!</button>
+    </nav>
+  );
+};
+
+export default GetWordBtn;
